e9dba4a1329e93e0e355f86a5627033b9882830b
Michael Halstead <halstead@happypunch.com>
2013-05-11 15:11:23 -0700

    Added sounds.

--- eat_a_snake.py
+++ eat_a_snake.py
@@ -4,6 +4,9 @@ from pygame.locals import *
 pygame.init()
 fpsClock = pygame.time.Clock()
 
+soundObj = pygame.mixer.Sound('slither.wav')
+hunterSound = pygame.mixer.Sound('hunt.wav')
+
 windowSurfaceObj = pygame.display.set_mode((1008,630))
 catSurfaceObj = pygame.image.load('background.jpg')
 snakeLeft = pygame.image.load('snake_left.png').convert()
@@ -18,7 +21,7 @@ hunterRight.set_colorkey(snakeLeft.get_at((0,0)))
 
 snakePOS = (0,0)
 
-choice = range(-5,5) 
+choice = range(-5,6) 
 change = (random.choice(choice),random.choice(choice))
 hchange = (random.choice(choice),random.choice(choice))
 windowSurfaceObj.blit(catSurfaceObj, (0,0))
@@ -32,6 +35,7 @@ while True:
     if random.randint(0,10) == 10:
         change = (random.choice(choice),random.choice(choice))
     snakePOS = (snakePOS[0]+change[0],snakePOS[1]+change[1])
+    
     if snakePOS[0] < 0:
         snakePOS = (0,snakePOS[1])
     if snakePOS[1] < 0:
@@ -71,6 +75,13 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
-                
+
+    dist = snakePOS[0] - hunterPOS[0] / (snakePOS[1] - hunterPOS[1])
+    if dist > -1000 and soundObj.get_num_channels() == 0:
+        soundObj.play()
+    elif dist < -1000 and hunterSound.get_num_channels() == 0:
+        hunterSound.play()
+    print(dist)
     pygame.display.update()
     fpsClock.tick(30)
+#y1-x1/y2-x2
