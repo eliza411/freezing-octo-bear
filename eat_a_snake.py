a3a354369ab610e6efb478a74561876ae0647119
Michael Halstead <michael@yoctoproject.org>
2013-05-18 12:02:14 -0700

    Starting to use sprites.

--- eat_a_snake.py
+++ eat_a_snake.py
@@ -1,6 +1,33 @@
 import pygame, sys, random, math 
 from pygame.locals import *
 
+class InventoryItem(pygame.sprite.Sprite):
+    def __init__(self, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image)
+        self.POS = (random.randint(0,1008), random.randint(0,630))
+        self.rect = self.image.get_rect()
+    def setMovementMod(self, mod):
+        self.movemod = mod
+    def getMovementMod(self):
+        return self.movemod
+
+class Hunter(pygame.sprite.Sprite):
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, image):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = image
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
 pygame.init()
 fpsClock = pygame.time.Clock()
 
@@ -22,12 +49,19 @@ snakeLeft.set_colorkey(snakeLeft.get_at((0,0)))             #Choose one pixel an
 snakeRight.set_colorkey(snakeRight.get_at((0,0)))
 
 Inventory = pygame.image.load('Inventory.png')
+#Load inventory sprites
+invItems = []
+invItems.append(InventoryItem('RaccoonLeaf.gif'))
+
+invItems[0].setMovementMod(6)
 
 
 hunterLeft = pygame.image.load('ash_left.png').convert()    #Set hunter sprites
 hunterRight = pygame.image.load('ash_right.png').convert()
 hunterLeft.set_colorkey(snakeLeft.get_at((0,0)))            #Set hunter background transparency
 hunterRight.set_colorkey(snakeLeft.get_at((0,0)))
+
+hunter = Hunter(hunterLeft) #Hunter starts the game looking left
 
 snakePOS = (0,0)            #Initial snake position
 
@@ -38,11 +72,18 @@ windowSurfaceObj.blit(catSurfaceObj, (0,0))                 #Draw the background
 
 hunterMax = (1000-hunterLeft.get_size()[0], 630-hunterLeft.get_size()[1])   #Variable used to prevent Hunter from leaving screen
 hunterPOS = hunterMax   #Hunter's start position
-hunter = hunterLeft     #Hunter starts the game looking left
+
 
 snake = snakeLeft #Snake starts the game looking left
 
 control_direction = [0,0]   #variable used to move hunter
+invItemsPossessed = []
+
+
+spriteGroup = pygame.sprite.Group()
+for item in invItems:
+    spriteGroup.add(item)
+
 while True:
     windowSurfaceObj.blit(catSurfaceObj, (0,0))             #Constantly draw background
     oldPOS = snakePOS       #Snake's position of the last frame
@@ -50,7 +91,11 @@ while True:
         change = (random.choice(choice),random.choice(choice))
 
     windowSurfaceObj.blit(Inventory, (0,630))
+    for item in invItemsPossessed:
+        windowSurfaceObj.blit(item.image, (3,633))
 
+    for item in invItems:
+        windowSurfaceObj.blit(item.image, item.POS)
 
     #Snake stuffs    
     snakePOS = (snakePOS[0]+change[0],snakePOS[1]+change[1])
@@ -78,6 +123,9 @@ while True:
     if random.randint(0,10) == 10:
         hchange = (random.choice(choice),random.choice(choice))
     # Calculate the new position based on randomness plus the control direction
+    for item in invItemsPossessed:
+        if item.getMovementMod():
+            control_direction[0] += item.getMovementMod()
     hunterPOS = (hunterPOS[0]+hchange[0]+control_direction[0],hunterPOS[1]+hchange[1]+control_direction[1])
     if hunterPOS[0] > hunterMax[0]:
         hunterPOS = (hunterMax[0], hunterPOS[1])
@@ -89,11 +137,17 @@ while True:
         hunterPOS = (hunterPOS[0],0)
 
     if oldHPOS[0] > hunterPOS[0]:
-        hunter = hunterLeft
+        hunter.image = hunterLeft
     elif oldHPOS[0] < hunterPOS[0] :
-        hunter = hunterRight
-    windowSurfaceObj.blit(hunter, hunterPOS)
+        hunter.image = hunterRight
+    windowSurfaceObj.blit(hunter.image, hunterPOS)
     
+        
+
+    for item in pygame.sprite.spritecollide(hunter,spriteGroup,False):
+        print item
+        if item == invItems[0]:
+            sys.exit()
 
     for event in pygame.event.get():
         if event.type == QUIT:
