c3bda82872868bb6550d6e733f7e939155d74ec0
Michael Halstead <michael@yoctoproject.org>
2013-05-18 18:13:35 -0700

    Remove usused code.

--- eat_a_snake.py
+++ eat_a_snake.py
@@ -7,9 +7,6 @@ if __name__ == '__main__':
     pygame.init()
     fpsClock = pygame.time.Clock()
 
-
-
-
     snakeSound = pygame.mixer.Sound('assets/audio/slither.wav')
     hunterSound = pygame.mixer.Sound('assets/audio/hunt.wav')
 
@@ -20,71 +17,37 @@ if __name__ == '__main__':
     hunterChannel.play(hunterSound, -1)
     hunterChannel.pause()
 
-
     windowSurfaceObj = pygame.display.set_mode((1008,700))      #Set window size
     catSurfaceObj = pygame.image.load('assets/images/background.jpg')         #Set background sprite
-
-
-    img =  pygame.image.load('assets/images/seamless-tile.png')
-    xcatSurfaceObj = pygame.Surface((8000,6000))
-    for y in range(0,6000,400):
-        for x in range(0,8000,640):
-            xcatSurfaceObj.blit(img,(x,y))
-
-
-    snakeLeft = pygame.image.load('assets/images/snake_left.png').convert()   #Set Snake sprites
-    snakeRight = pygame.image.load('assets/images/snake_right.png').convert()
-    snakeLeft.set_colorkey(snakeLeft.get_at((0,0)))             #Choose one pixel and make all pixels that color transparent
-    snakeRight.set_colorkey(snakeRight.get_at((0,0)))
-
 
     hunter =  hunter.Hunter('assets/images/ash_left.png', 'assets/images/ash_right.png') #Hunter starts the game looking left
     AliveSprites = pygame.sprite.Group(hunter)
 
     Inventory = pygame.image.load('assets/images/Inventory.png')
     #Load inventory sprites
-    invItems = []
+    itemSprites = pygame.sprite.Group()
     for x in range(10):
         leaf = items.InventoryItem('assets/images/leaf.png')
-        invItems.append(leaf)
+        itemSprites.add(leaf)
+        AliveSprites.add(leaf)
         leaf.setMovementMod(6)
-        if hunter.rect.colliderect(leaf.rect):
+        if hunter.rect.colliderect(leaf.rect): #If we start with a collision move the leaf
             leaf.rect.x = random.randint(50, 950)
             leaf.rect.y = random.randint(50, 580)
 
-    choice = range(-5,6)        #Made a list of -5 to 5
-    change = (random.choice(choice),random.choice(choice))      
-    hchange = (random.choice(choice),random.choice(choice))
     windowSurfaceObj.blit(catSurfaceObj, (0,0))                 #Draw the background
 
-
-
-    snake = snake.Snake(snakeLeft) #Snake starts the game looking left
+    snake = snake.Snake() #Create one snake
     AliveSprites.add(snake)
 
-    control_direction = [0,0]   #variable used to move hunter
-    invItemsPossessed = []
-
-
-    spriteGroup = pygame.sprite.Group()
-    for item in invItems:
-        spriteGroup.add(item)
-        AliveSprites.add(item)
         
 
     while True:
-        windowSurfaceObj.blit(catSurfaceObj, (0,0))             #Constantly draw background
-        if random.randint(0,10) == 10:
-            change = (random.choice(choice),random.choice(choice))
-
-        windowSurfaceObj.blit(Inventory, (0,630))
-        for item in invItemsPossessed:
-            windowSurfaceObj.blit(item.image, (3,633))
-            
-        collide =  pygame.sprite.spritecollide(hunter, spriteGroup, True)
+        windowSurfaceObj.blit(catSurfaceObj, (0,0)) #Constantly draw background
+        windowSurfaceObj.blit(Inventory, (0,630)) #Draw the iventory boxes every refresh
+        collide =  pygame.sprite.spritecollide(hunter, itemSprites, True)
         if collide:
             hunter.inventory.add(collide)
-
 
         AliveSprites.draw(windowSurfaceObj) 
         hunter.inventory.draw(windowSurfaceObj)
@@ -144,6 +107,4 @@ if __name__ == '__main__':
                     hunter.movey = 0
 
         pygame.display.flip()
-        
         fpsClock.tick(FPS)
-    #y1-x1/y2-x2
--- snake.py
+++ snake.py
@@ -2,17 +2,17 @@ import pygame, sys, random, math
 from pygame.locals import *
 
 class Snake(pygame.sprite.Sprite):
-    def __init__(self, image):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.choice = range(-5,6)        #Made a list of -5 to 5
-        self.change = (0,0)
-
         self.snakeLeft = pygame.image.load('assets/images/snake_left.png').convert()   #Set Snake sprites
         self.snakeRight = pygame.image.load('assets/images/snake_right.png').convert()
         self.snakeLeft.set_colorkey(self.snakeLeft.get_at((0,0)))             #Choose one pixel and make all pixels that color transparent
         self.snakeRight.set_colorkey(self.snakeRight.get_at((0,0)))
+        self.image = self.snakeLeft #Snake starts the game looking left
+        self.rect = self.image.get_rect()
+        self.choice = range(-5,6)        #Made a list of -5 to 5
+        self.change = (0,0)
+
     def update(self):
         self.move(self.change[0], self.change[1])
         if random.randint(0,10) == 10:
